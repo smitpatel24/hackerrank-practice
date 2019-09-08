0fd1fa85949e53a6692a340e8f74628fc4516832
smitpatel24 <smit@gopikunj.com>
2019-09-08 10:30:09 +0530

    Added divisible-sum-pairs.cpp

---
+++ divisible-sum-pairs.cpp
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+// Complete the divisibleSumPairs function below.
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if((ar[i] + ar[j]) % k == 0)
+                count++;
+        }
+    }
+    return count;
+}
